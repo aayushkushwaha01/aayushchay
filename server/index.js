9c33a7b88f70b629260ce836301f4018150eb520
aayushkushwaha01 <156602395+aayushkushwaha01@users.noreply.github.com>
2024-01-22 01:21:12 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://aayushchay-qeij.vercel.app/login",
     credentials: true,
   },
 });
